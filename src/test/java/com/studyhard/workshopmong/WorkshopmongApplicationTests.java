116b17132645ed93830bc1d22aae62ffc9e0e43c
Wallace Tavares Oliveira <wallacetavaresoliveira84@gmail.com>
2022-04-17 11:51:57 -0300

    Project created

---
+++ src/test/java/com/studyhard/workshopmong/WorkshopmongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyhard.workshopmong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
